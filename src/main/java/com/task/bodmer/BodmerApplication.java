50f34786d2c6df41d50648369a398daeb3505328
IBodmer <pewpew308@gmail.com>
2023-01-12 13:58:33 +0300

    Initial commit

---
+++ src/main/java/com/task/bodmer/BodmerApplication.java
@@ -0,0 +1,13 @@
+package com.task.bodmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodmerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/bodmer/BodmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.bodmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
