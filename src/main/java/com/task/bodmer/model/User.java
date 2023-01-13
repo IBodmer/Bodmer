01ec6b3a17ef8758e0aaeca761ddd0128d1c0bbe
IBodmer <pewpew308@gmail.com>
2023-01-13 13:14:20 +0300

    Bdm-2 Create model + migration + compose

---
+++ src/main/java/com/task/bodmer/model/User.java
@@ -0,0 +1,39 @@
+package com.task.bodmer.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(name="id")
+    private Long id;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role_in_project")
+    private RoleProject roleProject;
+    @Column(name = "registered_at")
+    private LocalDateTime created;
+//TODO дописать поле с коллекцией проектов и задач, а так же добавить все в миграцию, поменять название ролей в проекте
+
+}
---
+++ src/main/resources/db.migration/V1__model.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "user"
+(
+    "id"              int8 PRIMARY KEY not null ,
+    "user_name"       varchar(50)not null,
+    "name"            varchar(50),
+    "last_name"       varchar(50),
+    "email"           varchar(50)not null,
+    "password"        varchar(255)not null,
+    "role"            varchar(30),
+    "role_in_project" varchar(30),
+    "registered_at"   timestamp
+);
